928805f005c4ca40fa0d2ee4dd71b32ecab5d1b4
BrunoMilosevic <brunomilosevic1234@gmail.com>
2023-10-16 11:51:00 +0200

    deleted node_modules

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 import "@popperjs/core";
 import * as bootstrap from "bootstrap";
 import "@/scss/main.scss";
+import ScrollReveal from "scrollreveal";
+
+const sr = ScrollReveal();
+
+sr.reveal(".progress-bar", {
+  origin: "left",
+  duration: 2500,
+  distance: "100%",
+  easing: "ease",
+  scale: 1,
+  mobile: true,
+  reset: true,
+});
+sr.reveal(".about-in", {
+  origin: "left",
+  duration: 2500,
+  distance: "100%",
+  easing: "ease",
+  scale: 1,
+  mobile: true,
+  reset: true,
+});
+sr.reveal(".portfolio-div", {
+  origin: "left",
+  duration: 2500,
+  distance: "100%",
+  easing: "ease",
+  scale: 1,
+  mobile: true,
+  reset: true,
+});
